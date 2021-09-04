57a56193e904a985d8f3ca3d15d4cf701bb51906
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2021-09-04 02:50:42 +0200

    Seq test 3

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,11 +1,11 @@
-use proc_macro::TokenStream;
+use proc_macro2::{Group, Literal, TokenStream, TokenTree};
 use syn::{braced, parse::Parse, parse_macro_input, Token};
 
 struct SeqItem {
-    _ident: syn::Ident,
-    _start: syn::LitInt,
-    _end: syn::LitInt,
-    _body: proc_macro2::TokenStream,
+    ident: syn::Ident,
+    start: syn::LitInt,
+    end: syn::LitInt,
+    body: TokenStream,
 }
 
 impl Parse for SeqItem {
@@ -20,17 +20,56 @@ impl Parse for SeqItem {
         let body = content.parse()?;
 
         Ok(SeqItem {
-            _ident: ident,
-            _start: start,
-            _end: end,
-            _body: body,
+            ident,
+            start,
+            end,
+            body,
         })
     }
 }
 
-#[proc_macro]
-pub fn seq(input: TokenStream) -> TokenStream {
-    let _seq_item = parse_macro_input!(input as SeqItem);
+fn map_identifier_recursive(
+    expected_ident: &syn::Ident,
+    replacement: &TokenTree,
+    tree: TokenTree,
+) -> TokenTree {
+    match tree {
+        TokenTree::Ident(ident) if ident == *expected_ident => replacement.clone(),
+        TokenTree::Group(group) => {
+            let mut stream = TokenStream::new();
+            stream.extend(
+                group
+                    .stream()
+                    .clone()
+                    .into_iter()
+                    .map(|token| map_identifier_recursive(expected_ident, replacement, token)),
+            );
 
-    TokenStream::new()
+            let new_group = Group::new(group.delimiter(), stream);
+            TokenTree::Group(new_group)
+        }
+        tt => tt,
+    }
+}
+
+#[proc_macro]
+pub fn seq(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let seq_item = parse_macro_input!(input as SeqItem);
+
+    let start: u32 = seq_item.start.base10_parse().unwrap();
+    let end: u32 = seq_item.end.base10_parse().unwrap();
+    let body = seq_item.body;
+    let loop_ident = seq_item.ident;
+
+    let mut stream = TokenStream::new();
+    for i in start..end {
+        let replacement_identifier = TokenTree::Literal(Literal::u32_unsuffixed(i));
+        stream.extend(
+            body.clone()
+                .into_iter()
+                .map(|token| map_identifier_recursive(&loop_ident, &replacement_identifier, token)),
+        );
+    }
+
+    stream.into()
 }
